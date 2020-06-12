89e3e8c6daa316f9b3ca427ba4cf260599fbae1a
Luis Fernando <luisfernando20140@outlook.com>
2020-06-12 14:28:34 -0300

    Projeto inciado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace construtorCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+            sapato.Nome = "Vizzano";
+            
+            Produto chinelo = new Produto(45);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            Produto camisa = new Produto(34, "Nike", "Azul", 230);
+            System.Console.WriteLine($"{camisa.Codigo}, {camisa.Nome}, {camisa.Descricao}, {camisa.Estoque}");
+
+
+
+        }
+    }
+}
